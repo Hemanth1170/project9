b70950966db9a3f06886aa64d835e4622c20ea1a
Hemanth1170 <130034263+Hemanth1170@users.noreply.github.com>
2023-10-05 16:34:41 +0530

    Add files via upload

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.hemanth.apps.project9.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
